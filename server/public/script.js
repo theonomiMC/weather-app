8f171558e59de7020517dd56acd80869773df4cd
Natalia bakakuri <64376039+theonomiMC@users.noreply.github.com>
2021-02-19 17:20:38 +0400

    Update script.js

--- server/public/script.js
+++ server/public/script.js
@@ -1,38 +1,45 @@
 // SELECT INPUT FORM
 const inputText = document.getElementById('query');
 let cityName = inputText.value;
-// const form = document.getElementById('formInput')
-inputText.addEventListener('input', e=> {
+const form = document.getElementById('formInput')
+inputText.addEventListener('input', e => {
   cityName = e.target.value;
   console.log(cityName)
 });
 
-btn.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   console.log('submited');
 
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=193800cfd4e466a915d90441543f6446&units=metric`)
-    .then(response => response.json())
-    .then(data => {
-      let city = document.getElementById("city");
-      let description = document.getElementById("desc");
-      let temperatur = document.getElementById("temp");
-      let wind = document.getElementById("wind");
-      let weatherIcon = document.getElementById('weather-icon');
-      let errorMessage = document.getElementById('msg');
-      if (data.name) {
-        console.log(data.weather[0].icon)
-        city.innerText = data.name;
-        description.innerHTML = data.weather[0].description;
-        temperatur.innerHTML = Math.round(data.main.temp) + '°C';
-        wind.innerHTML = "speed: " + data.wind.speed + ",\ndeg: " + data.wind.deg
-        weatherIcon.innerHTML = `<img src=\"icons/${data.weather[0].icon}.png\" />`;
-
-      } else {
-        errorMessage.innerHTML = "This city is unknown"
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=614c82475666965970f1257189e30877&units=metric`)
+    .then(response => {
+      if (response.ok) {
+        console.log(response.ok)
+        return response.json()
       }
+      throw new Error('somthing went wrong!')
     })
-    .catch(error => console.log(error));
+    .then(data => {
 
-  inputText.value = '';
+      let city = document.querySelector(".CityNames");
+      let description = document.querySelector(".Description");
+      let temperatur = document.querySelector(".Temperatur");
+      let wind = document.querySelector(".Wind");
+      let weatherIcon = document.querySelector('#weather-icon');
+      let Weatherinfo = document.querySelector('.Weatherinfo');
+
+      console.log(data.weather[0].icon)
+      console.log(data.name)
+      city.innerHTML = data.name;
+      description.innerHTML = data.weather[0].description;
+      temperatur.innerHTML = Math.round(data.main.temp) + '°C';
+      wind.innerHTML = "speed: " + data.wind.speed + ",\ndeg: " + data.wind.deg
+      weatherIcon.innerHTML = `<img src=\"icons/${data.weather[0].icon}.png\" />`;
+    })
+    .catch(error => {
+      console.log(error)
+      Weatherinfo.innerHTML = "Connection faild, please try later .. ";
+    })
+  
+inputText.value = '';
 })
